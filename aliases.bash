7717190266d1a838479a280622d90e161b209644
Gagan Awhad <gagan.a.awhad@gmail.com>
2017-06-20 16:04:05 -0500

    Add alias for rails to aliases again

--- aliases.bash
+++ aliases.bash
@@ -20,7 +20,8 @@ alias gcis='git commit --allow-empty -m "[ci skip]"'
 # Aliases for vim
 alias removeunwanted='find . -name "*.swp" -print0 | xargs -0 rm -rf; find . -name "*.swn" -print0 | xargs -0 rm -rf; find . -name "*.swo" -print0 | xargs -0 rm -rf; find . -name "*.swm" -print0 | xargs -0 rm -rf; find . -name "*.swk" -print0 | xargs -0 rm -rf; find . -name "*.swl" -print0 | xargs -0 rm -rf'
 
-
+# Aliases for rails
+alias r='rails'
 
 # Aliases for grep
 alias grep='grep --color=auto'
